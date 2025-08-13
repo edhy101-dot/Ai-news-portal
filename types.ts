adf0c3077f6cb5f86c9d657c4b44c8083209a015
edhy101-dot <edhy101@gmail.com>
2025-08-13 11:57:16 +0700

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,12 @@
+export type FeedItem = {
+  id: string;
+  title: string;
+  link: string;
+  isoDate?: string;
+  pubDate?: string;
+  image?: string;
+  excerpt?: string;
+  source?: string;
+  category: string;
+  slug: string;
+};
